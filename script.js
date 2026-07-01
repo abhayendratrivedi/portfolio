a0bcde61ff0b5d9c90bacf77f94f839f354fed82
Abhay Trivedi <abhaytrivedi8318@gmail.com>
2026-07-01 01:36:45 -0700

    fixed script.js animations

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+// Typing Animation
+const typingText = document.querySelector('.highlight');
+const words = ['businesses', 'startups', 'restaurants', 'clinics', 'you'];
+let wordIndex = 0;
+let letterIndex = 0;
+let isDeleting = false;
+
+function type() {
+    const currentWord = words[wordIndex];
+
+    if (isDeleting) {
+        typingText.textContent = currentWord.substring(0, letterIndex - 1);
+        letterIndex--;
+    } else {
+        typingText.textContent = currentWord.substring(0, letterIndex + 1);
+        letterIndex++;
+    }
+
+    if (!isDeleting && letterIndex === currentWord.length) {
+        isDeleting = true;
+        setTimeout(type, 1500);
+        return;
+    }
+
+    if (isDeleting && letterIndex === 0) {
+        isDeleting = false;
+        wordIndex = (wordIndex + 1) % words.length;
+    }
+
+    setTimeout(type, isDeleting ? 80 : 120);
+}
+
+type();
+
 // Mobile Menu
 function toggleMenu() {
     const navMenu = document.querySelector('nav ul');
